b37295267e1f01e8e137b622fa51cf5aab1e861a
Сергей Шамбир <sergei.shambir@travelline.ru>
2025-05-04 11:29:10 +0300

    Доработал плагин: теперь он запрашивает путь к каталогу и сохраняет в нём файл article.md с неизменным содержимым

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 chrome.action.onClicked.addListener(async (tab) => {
-    // await chrome.scripting.executeScript({
-    //     target: { tabId: tab.id },
-    //     files: ["page_to_markdown.js"]
-    // });
-    const doc = URL.createObjectURL(new Blob(
-        ['Hello, Chrome downloads!'],
-        { type: 'text/markdown' }
-    ));
-    await chrome.downloads.download({
-        url: doc,
-        filename: 'article.md',
-        conflictAction: 'uniquify'
+    await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["page_to_markdown.js"]
     });
+    // const doc = URL.createObjectURL(new Blob(
+    //     ['Hello, Chrome downloads!'],
+    //     { type: 'text/markdown' }
+    // ));
+    // await chrome.downloads.download({
+    //     url: doc,
+    //     filename: 'article.md',
+    //     conflictAction: 'uniquify'
+    // });
 });
